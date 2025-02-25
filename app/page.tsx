2dd75c91ec44adef7f5c1717a4eee8911b594417
ajmal5410347 <ajmal@airtory.com>
2025-02-25 22:41:31 +0530

    Shadcn ui install

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <div className='main-heading'>Home Page</div>
+    <>
+      <div className='main-heading'>
+        Home Page
+      </div>
+      <Button >Button</Button>
+    </>
   )
 }
