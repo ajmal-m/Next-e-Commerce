6b89a58cbf783a6dbc393223b0332b7f5b82549d
ajmal5410347 <ajmal@airtory.com>
2025-02-27 22:44:04 +0530

    Product card added

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,26 +1,16 @@
 import React from 'react';
-import { Button } from '@/components/ui/button';
+import sampleData from '@/db/sample-data';
+import ProductList from '@/components/shared/product/product-list';
 
 export const metadata = {
   title:`Home`
 }
 
-const delay = (time: number) => {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      resolve(true)
-    }, time);
-  })
-}
 
 export default async  function Home() {
-  await delay(5000)
   return (
     <>
-      <div className='main-heading'>
-        Home Page
-      </div>
-      <Button>Button</Button>
+      <ProductList data={sampleData.products} title='Newest Arrivals'/>
     </>
   )
 }
