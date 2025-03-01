5d9eb14fa7cfecc14f9859887441973b5cb50e46
ajmal5410347 <ajmal@airtory.com>
2025-03-01 23:29:16 +0530

    Deploy

--- components/shared/product/product-images.tsx
+++ components/shared/product/product-images.tsx
@@ -1,11 +1,10 @@
 'use client';
-import React from 'react';
 import { useState } from 'react';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 
-export default function productImages({images}: {images: string[]}) {
-    const [current, setCurrent]  = useState(0);
+export default function ProductImages({images}: {images: string[]}) {
+  const [current, setCurrent]  = useState(0);
   return (
     <div className="space-y-4">
         <Image 
