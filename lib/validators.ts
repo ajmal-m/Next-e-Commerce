3824c8c37f71646f6a058575eae10c4fe18dd7b0
ajmal5410347 <ajmal@airtory.com>
2025-03-05 21:19:29 +0530

    Sign-up function updated

--- lib/validators.ts
+++ lib/validators.ts
@@ -24,3 +24,15 @@ export const signInFormSchema = z.object({
     email: z.string().email("Invalid email address"),
     password: z.string().min(6, 'Password must be atleast 6 characters')
 });
+
+
+// Schema for user signing up a user
+export const signUpFormSchema = z.object({
+    name: z.string().min(3, 'Name must be atleast 3 characters'),
+    email: z.string().email("Invalid email address"),
+    password: z.string().min(6, 'Password must be atleast 6 characters'),
+    confirmPassword: z.string().min(6, 'Confirm password must be atleast 6 characters')
+}).refine((data) => data.password === data.confirmPassword, {
+    message:"Password don't match",
+    path:["confirmPassword"]
+});
