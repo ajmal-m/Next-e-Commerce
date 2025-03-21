b888114d56ceb7753306d176d14a2a19933b2db3
ajmal5410347 <ajmal@airtory.com>
2025-03-21 09:07:53 +0530

    Payment validator added

--- lib/validators.ts
+++ lib/validators.ts
@@ -1,5 +1,6 @@
 import {  z} from 'zod';
 import { formatNumberWithDecimal } from './utils';
+import { PAYMENT_METHODS } from './constants';
 
 const currency =  z.string().refine( (value) => /^\d+(\.\d{2})?$/.test(formatNumberWithDecimal(Number(value))), 'Price Must have Two decimal places');
 
@@ -71,3 +72,14 @@ export const shippingAddressSchema = z.object({
     lat: z.string().optional(),
     lng: z.string().optional()
 });
+
+
+// Schema for Payment Methods
+export const paymentMethodSchema = z.object({
+    type: z.string().min(1, 'Payment method is required')
+}).refine((data) => PAYMENT_METHODS.includes(data.type),{
+    path:['type'],
+    message:"Invalid payment method"
+} );
+
+
