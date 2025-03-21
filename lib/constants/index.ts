ccad33b99c59915b97d7a4c40eb2382d0bdb3a6f
ajmal5410347 <ajmal@airtory.com>
2025-03-21 08:56:01 +0530

    Payment method constants added

--- lib/constants/index.ts
+++ lib/constants/index.ts
@@ -24,3 +24,7 @@ export const ShippingAddressDefaultValues = {
     lat:'',
     lng:''
 }
+
+
+export const PAYMENT_METHODS = process.env.PAYMENT_METHODS ? process.env.PAYMENT_METHODS.split(", ") : ['PayPal', 'Stripe', 'CashOnDelivery'];
+export const DEFAULT_PAYMENT_MATHOD = process.env.DEFAULT_PAYMENT_MATHOD ? process.env.DEFAULT_PAYMENT_MATHOD : 'PayPal';
