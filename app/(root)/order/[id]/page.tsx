01b97461201e7f570e907823df77e004db6d8475
ajmal5410347 <ajmal@airtory.com>
2025-03-22 22:32:05 +0530

    Order detail page added

---
+++ app/(root)/order/[id]/page.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from "next";
+import { getOrderById } from "@/lib/actions/order.actions";
+import { notFound } from "next/navigation";
+import { ShippingAddress } from "@/types"; 
+
+
+export const metadata: Metadata = {
+    title: 'Order Details'
+};
+
+export default async function OrderDetailPage(props: { 
+    params: Promise<{
+        id:string
+    }>
+}) {
+
+    const {id} = await props.params;
+
+    const order = await getOrderById(id);
+
+    if(!order) notFound();
+
+    
+  return (
+    <div>Detail</div>
+  )
+}
