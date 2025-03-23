d7b6941695197f6b93fb50d8ffdd6216e493ccdb
ajmal5410347 <ajmal@airtory.com>
2025-03-23 12:09:53 +0530

    Create payment function added

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -1,6 +1,47 @@
 const base = process.env.PAYPAL_APP_URL || 'https://api-m.sandbox.paypal.com';
 
-export const paypal = {};
+export const paypal = {
+    createOrder: async function createOrder(price: number){
+        const accessToken = await generateAccessToken();
+        const url = `${base}/v2/checkout/orders`;
+
+        const response = await fetch(url, {
+            method:"POST",
+            headers:{
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+            body:JSON.stringify({
+                "intent": "CAPTURE",
+                "purchase_units":[
+                    {
+                        amount:{
+                            currency_code:'USD',
+                            value:price
+                        }
+                    }
+                ]
+            })
+        });
+
+        return handleResponse(response);
+    },
+
+    capturePayment:async function capturePayment(orderId: string){
+        const accessToken = await generateAccessToken();
+        const url = `${base}/v2/checkout/orders/${orderId}/capture`;
+
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${accessToken}`
+            },
+        });
+
+        return handleResponse(response);
+    }
+};
 
 // Generate paypal access token
 export async function generateAccessToken(){
@@ -16,11 +57,16 @@ export async function generateAccessToken(){
         }
     });
 
+    const jsonData = await handleResponse(response);
+    return jsonData.access_token;
+};
+
+
+const handleResponse = async (response: Response) => {
     if(response.ok){
-        const jsonData = await response.json();
-        return jsonData.access_token;
+        return response.json();
     }else{
         const errorMessage = await response.text();
         throw new Error(errorMessage);
     }
-};
+}
