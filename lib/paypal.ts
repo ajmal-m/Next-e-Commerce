cb5769fdc5337917b7459f150c9824edc23b6c0c
ajmal5410347 <ajmal@airtory.com>
2025-03-23 11:35:23 +0530

    Jest testing added

--- lib/paypal.ts
+++ lib/paypal.ts
@@ -3,7 +3,7 @@ const base = process.env.PAYPAL_APP_URL || 'https://api-m.sandbox.paypal.com';
 export const paypal = {};
 
 // Generate paypal access token
-async function generateAccessToken(){
+export async function generateAccessToken(){
     const {PAYPAL_CLIENT_ID, PAYPAL_APP_SECRET} = process.env;
     const auth = Buffer.from(`${PAYPAL_CLIENT_ID}:${PAYPAL_APP_SECRET}`).toString('base64');
 
@@ -23,4 +23,4 @@ async function generateAccessToken(){
         const errorMessage = await response.text();
         throw new Error(errorMessage);
     }
-}
+};
