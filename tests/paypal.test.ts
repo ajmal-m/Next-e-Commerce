58545427a234c81f36134654d9b480a9633242bd
ajmal5410347 <ajmal@airtory.com>
2025-03-23 12:53:10 +0530

    Capture payment test

--- tests/paypal.test.ts
+++ tests/paypal.test.ts
@@ -1,4 +1,4 @@
-import { generateAccessToken } from "../lib/paypal";
+import { generateAccessToken, paypal } from "../lib/paypal";
 
 // Test to generate Access Token from paypal
 test("access token from paypal",async () => {
@@ -6,4 +6,33 @@ test("access token from paypal",async () => {
     console.log(tokenResponse);
     expect(typeof tokenResponse).toBe('string');
     expect(tokenResponse.length).toBeGreaterThan(0);
+});
+
+// Test to Create paypal order
+test("create a paypal order", async () => {
+    const token = await generateAccessToken();
+    const price = 10.5;
+
+    const orderResponse = await paypal.createOrder(price);
+
+    console.log(orderResponse);
+
+    expect(orderResponse).toHaveProperty('id');
+    expect(orderResponse).toHaveProperty('status');
+    expect(orderResponse.status).toBe('CREATED');
+    
+});
+
+
+// Test to capture payment with mock order
+test('simulating captuaring payment from a order', async()=> {
+    const orderId = '100';
+    const capturePayment = jest.spyOn(paypal, 'capturePayment').mockResolvedValue({
+        status:"COMPLETED"
+    });
+
+    const captureResponse = await paypal.capturePayment(orderId);
+    console.log(captureResponse);
+    expect(captureResponse).toHaveProperty('status', 'COMPLETED');
+    capturePayment.mockRestore();
 });
