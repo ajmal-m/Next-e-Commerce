708d7b51ecc4ec9b560407abb3974c07d25f4282
ajmal5410347 <ajmal@airtory.com>
2025-03-24 21:58:08 +0530

    User update function added

--- lib/validators.ts
+++ lib/validators.ts
@@ -108,9 +108,17 @@ export const insertOrderItemSchema = z.object({
 });
 
 
+
+// Schema for for Paypal Result
 export const paymentResultSchema = z.object({
     id: z.string(),
     status: z.string(),
     email_address: z.string(),
     pricePaid: z.string(),
+});
+
+// Schema for updating user profile
+export const updateProfileSchema = z.object({
+    name: z.string().min(3, 'name atleast 3 characters.'),
+    email: z.string().min(3, 'Email must be atleast 3 characters.')
 });
