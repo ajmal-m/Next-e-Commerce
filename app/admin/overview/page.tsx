a2ff128b0308baffb49313e01c94fd1c511eafd1
ajmal5410347 <ajmal@airtory.com>
2025-03-25 08:51:06 +0530

    Admin layout and navbar added

---
+++ app/admin/overview/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function AdminOverviewPage() {
+  return (
+    <div>
+        <h1>Admin Overview Page</h1>
+    </div>
+  )
+}
--- components/shared/header/user-button.tsx
+++ components/shared/header/user-button.tsx
@@ -68,6 +68,16 @@ export default async function UserButton() {
                         </Link>
                     </DropdownMenuItem>
 
+                    {
+                        session?.user?.role === 'admin' && (
+                            <DropdownMenuItem>
+                                <Link href={'/admin/overview'} className='w-full'>
+                                    Admin
+                                </Link>
+                            </DropdownMenuItem>
+                        )
+                    }
+
                     <DropdownMenuItem className='p-0 mb-1'>
                         <form action={signOutUser} className='w-full'>
                             <Button className='w-full py-4 px-2 h-4 justify-start' variant={'ghost'}>
