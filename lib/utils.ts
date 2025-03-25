5903cda87dc6401475afa744d9a626dc4871c85d
ajmal5410347 <ajmal@airtory.com>
2025-03-25 22:05:14 +0530

    Admin Overview page added

--- lib/utils.ts
+++ lib/utils.ts
@@ -134,4 +134,12 @@ export function formUrlQuery({
   },{
     skipNull:true
   });
+};
+
+
+// Format Number
+const NUMBER_FORMATTER = new Intl.NumberFormat('en-US');
+
+export function formatNumber(number : number){
+  return NUMBER_FORMATTER.format(number);
 }
