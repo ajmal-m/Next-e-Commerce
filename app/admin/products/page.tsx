f4a5b5a90b33212e69cb41600090a94d4640bd0c
ajmal5410347 <ajmal@airtory.com>
2025-04-05 09:04:50 +0530

    admin products pagination update

--- app/admin/products/page.tsx
+++ app/admin/products/page.tsx
@@ -98,7 +98,7 @@ export default async function  AdminProductPage(props: {
         </TableBody>
     </Table>
     {
-        products?.totalPages && products?.totalPages > 1 && (
+        products?.totalPages > 1 && (
             <Pagination page={page} totalPages={products.totalPages}/>
         )
     }
