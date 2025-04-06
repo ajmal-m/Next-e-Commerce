2c4ca0d60856d5c958e63d0be03755f98ef227b0
ajmal5410347 <ajmal@airtory.com>
2025-04-06 12:09:27 +0530

    Category Drawer added

--- components/shared/header/index.tsx
+++ components/shared/header/index.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import Image from 'next/image';
 import { APP_NAME } from '@/lib/constants';
 import Menu from './menu';
+import CategoryDrawer from './category-drawer';
 
 
 export default function Header() {
@@ -10,6 +11,7 @@ export default function Header() {
     <header className="w-full border-b">
         <div className="wrapper flex-between">
             <div className="flex-start">
+                <CategoryDrawer/>
                 <Link href='/' className='flex-start'>
                     <Image src={'/images/logo.svg'} alt={`${APP_NAME} logo`} width={48} height={48} priority={true}/>
                     <span className="hidden lg:block font-bold ml-3 text-2xl">{APP_NAME}</span>
